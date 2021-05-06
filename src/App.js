c0dcbdb9833e89d090f3c89989d57d938f34f141
ChrisSnowman <csnowman129@gmail.com>
2021-05-05 17:56:05 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from './router/index'
+import Grad from './components/Grad'
 
-export default function App() {
- 
+export default function App(props) {
+
   return (
+    <div>
+    <Grad/>
     <AppRouter/>
+    </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import routes from "../router/routes";
-import { Link } from "react-router-dom";
+import { HashRouter, Route, Switch, Link } from "react-router-dom";
 import { Transition } from "@headlessui/react";
 
 export default function Nav(props) {
   const [open, setOpenss] = useState(false);
 
   return (
-    <div id="nav" key='245'>
+    <div id="nav" key="245">
       <input type="checkbox" className="hidden" id="navi-toggle" />
       <label
         htmlFor="navi-toggle"
         id="navigation__button"
         onClick={() => setOpenss((open) => !open)}
         style={{ backgroundColor: "rgba(255,247,231,0.5)" }}
-        className="h-14 w-14 fixed top-5 right-4 rounded-full shadow-xl"
+        className="h-14 w-14 absolute top-3 right-4 rounded-full shadow-xl"
       >
         <span
           style={{ marginTop: "5rem" }}
@@ -28,28 +28,33 @@ export default function Nav(props) {
         as="nav"
         enter="transform transition duration-500"
         enterFrom="-translate-x-72"
-        enterTo='translate-x-0'
-        leave='transform transition duration-500'
-        leaveFrom='translate-x-0'
-        leaveTo='-translate-x-72'
+        enterTo="translate-x-0"
+        leave="transform transition duration-500"
+        leaveFrom="translate-x-0"
+        leaveTo="-translate-x-72"
         id="vertical"
         className="bg-white overflow-hidden bg-opacity-30 backdrop-filter backdrop-blur-sm backdrop-saturate-50 absolute rounded-tr-3xl rounded-br-3xl shadow-sm py-32 -top-0 h-screen w-72"
       >
-        {routes
-          .filter((item) => !item.path.includes(":"))
-          .map(({ key, path }) => (
-            <Link
-              className={
-                open
-                  ? "flex flex-cols ml-3 font-light text-white my-6 py-2 text-2xl rounded-t-xl rounded-b-md w-full transition duration-700 ease-in-out transform hover:translate-x-14"
-                  : "flex flex-cols ml-3 w-full h-screen my-6 py-2 text-2xl"
-              }
-              key={key}
-              to={path}
-            >
-              {key}
-            </Link>
-          ))}
+        <HashRouter>
+          <Switch>
+            
+            {routes
+              .filter((item) => !item.path.includes(":"))
+              .map(({ key, path }) => (
+                <Link
+                  className={
+                    open
+                      ? "flex flex-cols ml-3 font-light text-white my-6 py-2 text-2xl rounded-t-xl rounded-b-md w-full transition duration-700 ease-in-out transform hover:translate-x-14"
+                      : "flex flex-cols ml-3 w-full h-screen my-6 py-2 text-2xl"
+                  }
+                  key={key}
+                  to={path}
+                >
+                  {key}
+                </Link>
+              ))}
+          </Switch>
+        </HashRouter>
       </Transition>
     </div>
   );
